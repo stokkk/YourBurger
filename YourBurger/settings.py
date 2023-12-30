4b2b9a8b9e8f17fabfc2d32ff4303bc40b38c825
Denis Gudoyt <gudoyt99@gmail.com>
2023-12-30 15:47:39 +0300

    Установлена БД postgresql Изменен gitignore (добавлено правило исключения каталога __pycache__ Добавлен суперпользователь

--- YourBurger/settings.py
+++ YourBurger/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'YourBurger.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'burger_db',
+        'USER': 'lyah',
+        'PASSWORD': 'lyah',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
@@ -104,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
